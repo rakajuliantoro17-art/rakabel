cd762e58f27afdcc535cc232b9453c42527e0a05
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-04-06 11:43:36 +0700

    Create customSchedule.js

---
+++ config/customSchedule.js
@@ -0,0 +1,9 @@
+// customSchedule.js
+export const todayCustomSchedule = [
+  { start: "07:00", end: "07:45", name: "JP 1", type: "JP", jp: 1 },
+  { start: "07:45", end: "08:30", name: "JP 2", type: "JP", jp: 2 },
+  { start: "08:30", end: "08:45", name: "Istirahat Pagi", type: "BREAK" },
+  { start: "08:45", end: "09:30", name: "JP 3", type: "JP", jp: 3 },
+  { start: "09:30", end: "10:15", name: "JP 4", type: "JP", jp: 4 },
+  { start: "10:15", end: "11:00", name: "Ekstra Kreatif", type: "EXTRA" },
+];
